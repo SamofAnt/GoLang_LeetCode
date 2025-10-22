70a01c2f9c4a340bf1c4be19c2ef159620c12b7f
Anton Samofalov <asamofalov@myseneca.ca>
2025-10-22 02:23:10 -0400

    Initial commit and added first task

---
+++ TwoSum/twoSum.go
@@ -0,0 +1,16 @@
+﻿package TwoSum
+
+func twoSum(nums []int, target int) []int {
+	hashTable := make(map[int]int)
+
+	for i, val := range nums {
+		comp := target - val
+
+		if idx, isFound := hashTable[comp]; isFound {
+			return []int{i, idx}
+		}
+		hashTable[val] = i
+	}
+
+	return []int{-1, -1}
+}
